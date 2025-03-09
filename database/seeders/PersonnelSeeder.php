e9fd5292e0286beef81bc31381cf0c14eccec08b
Tum <baleethai@gmail.com>
2025-03-09 14:36:31 +0700

    Comment out Personnel seeder data for future reference

--- database/seeders/PersonnelSeeder.php
+++ database/seeders/PersonnelSeeder.php
@@ -17,21 +17,21 @@ class PersonnelSeeder extends Seeder
      */
     public function run()
     {
-        Personnel::create([
-            'prefix_id' => 1,
-            'position_id' => 1,
-            'code' => '000000001',
-            'first_name' => 'สมชาย',
-            'last_name' => 'ใจดี',
-            'id_card' => '1231322331230',
-            'birthdate' => '1988-10-10',
-            'work_start_date' => '2020-10-10',
-            'bio' => 'xx',
-            'email' => 'somchai@x.com',
-            'password' => '1234',
-            'work_status' => WorkStatus::PERFORMWORK,
-            'sort' => 0,
-            'is_visible' => VisibleStatus::IS_VISIBLE,
-        ]);
+        // Personnel::create([
+        //     'prefix_id' => 1,
+        //     'position_id' => 1,
+        //     'code' => '000000001',
+        //     'first_name' => 'สมชาย',
+        //     'last_name' => 'ใจดี',
+        //     'id_card' => '1231322331230',
+        //     'birthdate' => '1988-10-10',
+        //     'work_start_date' => '2020-10-10',
+        //     'bio' => 'xx',
+        //     'email' => 'somchai@x.com',
+        //     'password' => '1234',
+        //     'work_status' => WorkStatus::PERFORMWORK,
+        //     'sort' => 0,
+        //     'is_visible' => VisibleStatus::IS_VISIBLE,
+        // ]);
     }
 }
